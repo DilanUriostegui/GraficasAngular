5d3d83a954d1c70d6900e5a42b176a19f596c1ad
Dilan <dilan.uriostegui@gmail.com>
2021-05-21 14:20:23 -0500

    Implementacion del grafico lineal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { ChartsModule } from 'ng2-charts';
 
 
 import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent
   ],
   imports: [
     BrowserModule,
